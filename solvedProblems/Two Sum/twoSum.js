fe1bf546c7771fb4d56651b3fa76b5f71b89623a
Aryan Batra <135307006+aryanbatras@users.noreply.github.com>
2026-01-22 08:24:36 +0530

    Update twoSum.js in Two Sum

---
+++ solvedProblems/Two Sum/twoSum.js
@@ -0,0 +1,13 @@
+function twoSum(nums, target) {
+  const numDict = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (complement in numDict) {
+      return [numDict[complement], i];
+    }
+    numDict[nums[i]] = i;
+  }l
+  return [];
+}
+
+console.log(twoSum([2, 6, 1, 5], 8));
